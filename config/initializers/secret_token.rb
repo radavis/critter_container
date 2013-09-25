8da3f72753ce8d0f8b7e0116db797bb2abf5300d
Richard Davis <rdavis.bacs@gmail.com>
2013-09-25 15:05:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CritterContainer::Application.config.secret_key_base = 'd9e0320f7684c2de6e19dd5246a07219e0a0a4a87ee961c717782d8317fe8301eb2e85edf39bf57e69bb8e221bd29fe1b6e61126abc2c1f7460b786460b37e3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CritterContainer::Application.config.session_store :cookie_store, key: '_critter_container_session'
