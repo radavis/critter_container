f3d0907d32f88f719e351e3e1cc9d5764b1e823e
akainic <annekainic@gmail.com>
2013-09-28 11:29:55 -0400

    Changed alias to username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:alias]
+    devise_parameter_sanitizer.for(:sign_up) << [:username]
   end
 
 end
--- db/migrate/20130927171651_devise_create_users.rb
+++ db/migrate/20130927171651_devise_create_users.rb
@@ -3,7 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
-      t.string :alias, null:false
+      t.string :username, null:false
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
