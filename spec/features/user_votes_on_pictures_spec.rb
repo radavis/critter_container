51f34c5395dddc7391e838b723d7955dcca0903d
akainic <annekainic@gmail.com>
2013-09-28 14:00:53 -0400

    started user votes spec, not yet complete

---
+++ spec/features/user_votes_on_pictures_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'user votes on pictures', %Q{
+  As a registered critter container user
+  I would like to vote on the pictures I like and dislike
+  So that the hivemind can decide the best pictures
+} do
+
+  # Acceptance Criteria:
+  #
+  # * There is an upvote button.
+  # * There is a downvote button.
+  # * User can only vote once.
+  # * User can change vote from positive to negative
+
+  scenario 'user upvotes a picture' do
+    
+  end  
+
+
+end
