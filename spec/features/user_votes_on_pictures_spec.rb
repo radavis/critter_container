bd77c30c0d5083410de5ecbeb1fceb6dcc2eb6f6
Richard Davis <rdavis.bacs@gmail.com>
2013-09-29 15:47:23 -0400

    generated Picture model as a placeholder

--- spec/features/user_votes_on_pictures_spec.rb
+++ spec/features/user_votes_on_pictures_spec.rb
@@ -14,8 +14,40 @@ feature 'user votes on pictures', %Q{
   # * User can change vote from positive to negative
 
   scenario 'user upvotes a picture' do
-    
-  end  
+    picture = FactoryGirl.create(:picture)  # this doesn't exist yet, but it will
+    previous_count = picture.vote.sum
 
+    visit picture_path(picture.id)
+    click_on 'up_arrow' # upvote id tag
+    expect(picture.vote.sum).to eql(previous_count + 1)
+  end
 
+  scenario 'user downvotes a picture' do
+    picture = FactoryGirl.create(:picture)
+    previous_count = picture.vote.sum
+
+    visit picture_path(picture.id)
+    click_on 'down_arrow'
+    expect(picture.vote.sum).to eql(previous_count - 1)
+  end
+
+  scenario 'user tries to vote twice' do
+    picture = FactoryGirl.create(:picture)
+    previous_count = picture.vote.sum
+
+    visit picture_path(picture.id)
+    click_on 'up_arrow'
+    click_on 'up_arrow'
+    expect(picture.vote.sum).to eql(previous_count + 1)
+  end
+
+  scenario 'user changes their vote' do
+    picture = FactoryGirl.create(:picture)
+    previous_count = picture.vote.sum
+
+    visit picture_path(picture.id)
+    click_on 'up_arrow'
+    click_on 'down_arrow'
+    expect(picture.vote.sum).to eql(previous_count - 1)
+  end
 end
