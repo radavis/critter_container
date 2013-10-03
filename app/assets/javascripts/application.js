e54b12d9165bd9f19b6d79875691ddffaa8574c3
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2013-10-03 16:43:00 -0400

    css/ajax votes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,29 @@
 var comment_toggle = function(id){
   $( "#comment_"+id ).slideToggle( 50, function() {} );
 }
+
+var submit_pic_upvote = function(id){
+   $.ajax({
+      type: "GET",
+      url: '/pictures/' + id + '/upvote.json',
+      dataType: 'json',
+      success: function(msg) {
+       alert('upvoted')
+      }
+    });
+  return false;
+  }
+  var submit_pic_downvote = function(id){
+   $.ajax({
+      type: "GET",
+      url: '/pictures/' + id + '/downvote.json',
+      dataType: 'json',
+      success: function(msg) {
+       alert('downvoted')
+      }
+    });
+  return false;
+  }
+
+
+
