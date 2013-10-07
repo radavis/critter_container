5093e1609eb10f811f725e505f670f4c47b984fe
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2013-10-07 11:27:19 -0400

    fixed everything

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,23 +1,24 @@
 var submit_pic_upvote = function(id){
-   $.ajax({
-      type: "GET",
-      url: '/pictures/' + id + '/upvote.json',
-      dataType: 'json',
-      success: function(msg) {
-       alert('upvoted')
-      }
-    });
+  $.ajax({
+    type: "GET",
+    url: '/pictures/' + id + '/upvote.json',
+    dataType: 'json',
+    success: function(msg) {
+      alert('upvoted');
+    }
+  });
   return false;
-  }
-  var submit_pic_downvote = function(id){
-   $.ajax({
-      type: "GET",
-      url: '/pictures/' + id + '/downvote.json',
-      dataType: 'json',
-      success: function(msg) {
-       alert('downvoted')
-      }
-    });
+};
+
+var submit_pic_downvote = function(id){
+  $.ajax({
+    type: "GET",
+    url: '/pictures/' + id + '/downvote.json',
+    dataType: 'json',
+    success: function(msg) {
+      alert('downvoted');
+    }
+  });
   return false;
-  }
+};
 
