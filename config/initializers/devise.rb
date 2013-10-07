45e8a3bdc551fe404104fe8cc81aace4b0a734e1
Ashley Basinger <ashley.hipps@gmail.com>
2013-10-07 15:10:36 -0400

    stil fixing secret key env variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,8 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  secret = ENV['CRITTERCONTAINER_DEVISE_SECRET']
-  Rails.logger.info(secret)
+  secret = ENV['CRITTERCONTAINER_SECRET']
   if secret.length < 30
     raise "Rails secret token cannot be loaded"
   else
